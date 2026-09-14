b8ddfbe1ac0d2a6610afd7adb0a16c72feec513a
johan253 <johannjo2000@gmail.com>
2026-09-13 21:35:02 -0700

    fix(db): typo in varchar type

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     token_hash VARCHAR NOT NULL UNIQUE,
-    ip_address VARCHAT NOT NULL,
+    ip_address VARCHAR NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
